d3e81edca345329feae4f18c34406dc9cd45b1f3
pgrondek <pgrondek@gmail.com>
2019-04-05 00:05:30 +0200

    Added bash_aliases

---
+++ bash_aliases
@@ -0,0 +1,5 @@
+alias home='cd ~'
+alias root='cd /'
+
+alias q=exit
+alias open=xdg-open
--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 rm -f $HOME/.vimrc
 rm -f $HOME/.bashrc
+rm -f $HOME/.bash_aliases
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
+ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 
 # Gnome
 dconf load /org/gnome/desktop/ < gnome/desktop.txt

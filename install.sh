833e8234be216abe83832fe947bf9f5455458b34
pgrondek <pgrondek@gmail.com>
2019-04-04 22:56:07 +0200

    Add bashrc

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 rm -f $HOME/.vimrc
+rm -f $HOME/.bashrc
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/bashrc $HOME/.bashrc
 
 # Gnome
 dconf load /org/gnome/desktop/ < gnome/desktop.txt

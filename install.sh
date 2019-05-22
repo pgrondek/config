65ae9f7d3c81b98561ea8e1a2d9de76005ca6f2d
Przemyslaw Grondek <przemyslaw.grondek@prudential.pl>
2019-05-22 11:47:01 +0200

    Added ideavimrc

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 rm -f $HOME/.vimrc
 rm -f $HOME/.bashrc
 rm -f $HOME/.bash_aliases
+rm -f $HOME/.ideavimrc
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
+ln -s $(pwd)/ideavimrc $HOME/.ideavimrc
 
 # Gnome
 dconf load /org/gnome/desktop/ < gnome/desktop.txt

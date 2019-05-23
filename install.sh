0cc5965d9e27dcf27e11dd48187bcdc17949c40f
pgrondek <pgrondek@gmail.com>
2019-05-23 22:21:46 +0200

    Added i3 configuration

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ rm -f $HOME/.vimrc
 rm -f $HOME/.bashrc
 rm -f $HOME/.bash_aliases
 rm -f $HOME/.ideavimrc
+rm -f $HOME/.i3/config
+
+mkdir -p $HOME/.i3/
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 ln -s $(pwd)/ideavimrc $HOME/.ideavimrc
+ln -s $(pwd)/i3-config $HOME/.i3/config
 
 # Gnome
 # dconf load /org/gnome/desktop/ < gnome/desktop.txt

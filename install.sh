6bfa49f9f47bb9803c051fb6bb5238ddc135a66a
pgrondek <pgrondek@gmail.com>
2019-05-23 22:34:19 +0200

    Add i3status config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ rm -f $HOME/.bashrc
 rm -f $HOME/.bash_aliases
 rm -f $HOME/.ideavimrc
 rm -f $HOME/.config/i3/config
+rm -f $HOME/.i3status.conf
 
 mkdir -p $HOME/.i3/
 
@@ -13,6 +14,7 @@ ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 ln -s $(pwd)/ideavimrc $HOME/.ideavimrc
 ln -s $(pwd)/i3-config $HOME/.config/i3/config
+ln -s $(pwd)/i3status.conf $HOME/.i3status.conf
 
 # Gnome
 # dconf load /org/gnome/desktop/ < gnome/desktop.txt

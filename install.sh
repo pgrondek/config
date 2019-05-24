c7fa2b929968edf9cd08a2cc883d3ebd74b5e69d
pgrondek <pgrondek@gmail.com>
2019-05-25 00:33:11 +0200

    [Dunst] Create configuration

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ rm -f $HOME/.bashrc
 rm -f $HOME/.bash_aliases
 rm -f $HOME/.ideavimrc
 rm -f $HOME/.config/i3/config
+rm -f $HOME/.config/dunst/dunstrc
 rm -f $HOME/.i3status.conf
 rm -f $HOME/bin/lock
 
 mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/bin/
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
@@ -17,6 +19,7 @@ ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 ln -s $(pwd)/ideavimrc $HOME/.ideavimrc
 ln -s $(pwd)/i3-config $HOME/.config/i3/config
 ln -s $(pwd)/i3status.conf $HOME/.i3status.conf
+ln -s $(pwd)/dunstrc $HOME/.config/dunst/dunstrc
 
 ln -s $(pwd)/scripts/lock $HOME/bin/lock
 

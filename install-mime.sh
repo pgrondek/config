764826dd6355114b94d34a0507fb9e2f43642235
pgrondek <pgrondek@gmail.com>
2019-05-31 00:21:19 +0200

    [MIME] Added handler for torrent files

--- install-mime.sh
+++ install-mime.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 sed -i '/x-scheme-handler\/magnet/d' $HOME/.config/mimeapps.list
 sed -i '/x-scheme-handler\/magnet/d' $HOME/.local/share/applications/mimeapps.list
+
+sed -i '/application\/x-bittorrent/d' $HOME/.config/mimeapps.list
+sed -i '/application\/x-bittorrent/d' $HOME/.local/share/applications/mimeapps.list
 
 cat $(pwd)/local/share/applications/mimeapps.list >> $HOME/.local/share/applications/mimeapps.list
 

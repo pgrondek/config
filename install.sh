2bafa9c8ff4612a5f13b414240407434143010ef
pgrondek <pgrondek@gmail.com>
2019-06-30 16:09:03 +0200

    [ssh] Added ssh authorized_keys

--- install.sh
+++ install.sh
@@ -12,10 +12,12 @@ rm -f $HOME/.i3status.conf
 rm -f $HOME/bin/lock
 rm -f $HOME/bin/transmission-add
 rm -f $HOME/.local/share/applications/torrent.desktop
+rm -f $HOME/.ssh/authorized_keys
 
 mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/bin/
+mkdir -p $HOME/.ssh
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
@@ -26,6 +28,9 @@ ln -s $(pwd)/i3blocks $HOME/.config/i3blocks
 ln -s $(pwd)/compton.conf $HOME/.config/compton.conf
 ln -s $(pwd)/dunstrc $HOME/.config/dunst/dunstrc
 
+ln -s $(pwd)/ssh/authorized_keys $HOME/.ssh/authorized_keys
+
 ln -s $(pwd)/bin/lock $HOME/bin/lock
 ln -s $(pwd)/bin/transmission-add $HOME/bin/transmission-add
 ln -s $(pwd)/local/share/applications/torrent.desktop $HOME/.local/share/applications/torrent.desktop
+

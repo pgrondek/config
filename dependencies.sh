0198eedff93ff17592e78ae606dc65e7ee4cb4f4
pgrondek <pgrondek@gmail.com>
2019-07-12 00:13:30 +0200

    [transmission] Move config to separate folder

--- dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-packages="transmission-cli"
-
-sudo aptitude install -y $packages
--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-rm -f $HOME/bin/transmission-add
-rm -f $HOME/.local/share/applications/torrent.desktop
 rm -f $HOME/.ssh/authorized_keys
 
-mkdir -p $HOME/bin/
 mkdir -p $HOME/.ssh
-mkdir -p $HOME/.local/share/applications
 
 ln -s $(pwd)/ssh/authorized_keys $HOME/.ssh/authorized_keys
-
-ln -s $(pwd)/bin/transmission-add $HOME/bin/transmission-add
-ln -s $(pwd)/local/share/applications/torrent.desktop $HOME/.local/share/applications/torrent.desktop
 

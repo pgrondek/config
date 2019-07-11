1ef8df42e6f065e4730dc6efc7377a8bab7daeb4
pgrondek <pgrondek@gmail.com>
2019-07-11 23:43:42 +0200

    [i3] Move all config to separate folder

--- install.sh
+++ install.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
 
-rm -f $HOME/.config/i3/config
-rm -f $HOME/.config/dunst/dunstrc
-rm -f $HOME/.config/i3blocks
-rm -f $HOME/.config/compton.conf
-rm -f $HOME/.i3status.conf
-rm -f $HOME/bin/lock
 rm -f $HOME/bin/transmission-add
 rm -f $HOME/.local/share/applications/torrent.desktop
 rm -f $HOME/.ssh/authorized_keys
 
-mkdir -p $HOME/.config/i3
-mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/bin/
 mkdir -p $HOME/.ssh
 mkdir -p $HOME/.local/share/applications
 
-ln -s $(pwd)/i3-config $HOME/.config/i3/config
-ln -s $(pwd)/i3blocks $HOME/.config/i3blocks
-ln -s $(pwd)/compton.conf $HOME/.config/compton.conf
-ln -s $(pwd)/dunstrc $HOME/.config/dunst/dunstrc
-
 ln -s $(pwd)/ssh/authorized_keys $HOME/.ssh/authorized_keys
 
-ln -s $(pwd)/bin/lock $HOME/bin/lock
 ln -s $(pwd)/bin/transmission-add $HOME/bin/transmission-add
 ln -s $(pwd)/local/share/applications/torrent.desktop $HOME/.local/share/applications/torrent.desktop
 

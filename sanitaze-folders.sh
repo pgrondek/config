4d3fcf3d37dd743deda73b9dbcf360108639b6fe
Przemysław Grondek <pgrondek@gmail.com>
2020-03-04 14:47:54 +0100

    Update sanitaze-folder

--- sanitaze-folders.sh
+++ sanitaze-folders.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ -d "${HOME}/Pobrane" ]
+#set -x
+set -e
+
+shopt -s dotglob nullglob
+
+if [[ -d "${HOME}/Pobrane" && ! -L "${HOME}/Pobrane" ]]
 then
     mkdir -p "${HOME}/Downloads"
     mv ${HOME}/Pobrane/* "${HOME}/Downloads"
@@ -9,11 +14,29 @@ then
     ln -s "${HOME}/Downloads" "${HOME}/Pobrane"
     echo "Symlinked ${HOME}/Pobrane to ${HOME}/Downloads"
 else
-    if [ -L "${HOME}/Pobrane" ]
+    if [[ -L "${HOME}/Pobrane" ]]
     then
         echo "Nothing to do ${HOME}/Pobrane is already symlink"
     else
         ln -s "${HOME}/Downloads" "${HOME}/Pobrane"
         echo "Symlinked ${HOME}/Pobrane to ${HOME}/Downloads"
+    fi
+fi
+
+if [[ -d "${HOME}/Dokumenty" && ! -L "${HOME}/Dokumenty" ]]
+then
+    mkdir -p "${HOME}/Documents"
+    mv ${HOME}/Dokumenty/* "${HOME}/Documents" || true
+    echo "Moved all download srom ${HOME}/Dokumenty to ${HOME}/Documents"
+    rmdir "${HOME}/Dokumenty"
+    ln -s "${HOME}/Documents" "${HOME}/Dokumenty"
+    echo "Symlinked ${HOME}/Dokumenty to ${HOME}/Documents"
+else
+    if [[ -L "${HOME}/Dokumenty" ]]
+    then
+        echo "Nothing to do ${HOME}/Dokumenty is already symlink"
+    else
+        ln -s "${HOME}/Documents" "${HOME}/Dokumenty"
+        echo "Symlinked ${HOME}/Dokumenty to ${HOME}/Documents"
     fi
 fi
